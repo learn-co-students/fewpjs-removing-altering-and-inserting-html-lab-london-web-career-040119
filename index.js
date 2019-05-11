37c289be4548ba2717b2330bd3d967b52320077f
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-11 22:20:01 +0100

    Practice changing DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main.querySelector('main#main'))
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = "Rebecca is the champion";
